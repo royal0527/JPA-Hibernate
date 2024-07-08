43d15c194af899bf6828582f18520372137382a3
rajusk0527 <rajas2096@gmail.com>
2024-07-09 01:24:46 +0530

    added h2 console and jdbc

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28Mintues/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,22 @@
+package com.in28Mintues.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private static String INSERT_QUERY =
+            """ 
+                    insert into course (id, name, author) values(1, 'Learn AWS', 'in28minutes')
+                    
+                    """;
+
+    public void insert(){
+        jdbcTemplate.update(INSERT_QUERY);
+    }
+
+}
